45c2b0a97af1ed5f785b54c6aefdf5b1c49d6265
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-05 17:00:47 -0800

    added Mirage to fake API until I can connect rails; added Language model

--- app/routes/languages.js
+++ app/routes/languages.js
@@ -1,27 +1,7 @@
 import Ember from 'ember';
 
-let languages = [
-  {
-    id: 1,
-    name: "Vafuna",
-    description: "some weird language"
-  },
-  {
-    id: 2,
-    name: "Nkalo",
-    description: "another weird language"
-  },
-  {
-    id: 3,
-    name: "Kelen",
-    description: "this language doesn't verb"
-  },
-
-
-];
-
 export default Ember.Route.extend({
   model() {
-    return languages;
+    return this.get('store').findAll('language');
   }
 });
---
+++ mirage/config.js
@@ -0,0 +1,30 @@
+export default function() {
+  this.namespace = '/api';
+
+  this.get('/languages', function(){
+    return {
+      data: [{
+        type: 'languages',
+        id: '1',
+        attributes: {
+          name: 'Vafuna',
+          description: 'some weird language',
+        }
+      }, {
+        type: 'languages',
+        id: '2',
+        attributes: {
+          name: 'Nkalo',
+          description: 'Some other language',
+        }
+      }, {
+        type: 'languages',
+        id: '3',
+        attributes: {
+          name: 'Kelen',
+          description: 'this language doesn\'t verb',
+        }
+      }]
+    };
+  });
+}

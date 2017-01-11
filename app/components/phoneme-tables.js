85e54f77d4fb590d3fb9bb85d11ae301958d1a05
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-11 13:15:47 -0800

    added click handler to change the class of the clicked item (still doesn't do anything on the back end

--- app/components/phoneme-tables.js
+++ app/components/phoneme-tables.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
-  
+  click(event) {
+    // var clicked = $(event.target).("td");
+    var clickedID = $(event.target).attr("id");
+    if (clickedID === undefined) {
+      clickedID = "ph-0";
+    }
+    var phoneID = parseInt(clickedID.slice(3));
+    console.log($(event.target));
+    console.log("clickedID: " + clickedID);
+    console.log("ID to add to lang: " + phoneID);
+    if(phoneID > 0) {
+      $(event.target).toggleClass("selected-phone");
+    }
+  }
 });

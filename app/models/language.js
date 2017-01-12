e6d0d143031a2149399b7c8093e83184926b3768
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-12 13:43:36 -0800

    added phoneme_ids to language model on EmbeR

--- app/models/language.js
+++ app/models/language.js
@@ -5,14 +5,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
   phonemes: DS.attr(),
+  phoneme_ids: DS.attr(),
 
-  isValid: Ember.computed.notEmpty('name'),
-  phonemeIds: Ember.computed('phonemes', function(){
-    var idArr = [];
-    var phonemes = this.get('phonemes');
-    for (var i = 0; i < phonemes.length; i++){
-      idArr.push(phonemes[i].id);
-    }
-    return idArr;
-  })
+  isValid: Ember.computed.notEmpty('name')
 });

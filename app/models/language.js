be9a95a95732137430133dc63cb0132f7f76265c
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-18 10:30:28 -0800

    trying to get phoneme model to work

--- app/models/language.js
+++ app/models/language.js
@@ -1,17 +1,17 @@
 import DS from 'ember-data';
-import Ember from 'ember';
+// import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  phonemes: DS.attr(),
+  phonemes: DS.hasMany('phoneme'),
   phoneme_ids: DS.attr(),
   words: DS.attr(),
 
-  consonants: Ember.computed.filterBy('phonemes', 'syllabic', false),
-  vowels: Ember.computed.filterBy('phonemes', 'syllabic', true),
-  isValid: Ember.computed.notEmpty('name'),
-  tds: Ember.computed.map('phoneme_ids', function(phoneme_id) {
-    return `#ph-${phoneme_id}`;
-  }),
+  // consonants: Ember.computed.filterBy('phonemes', 'syllabic', false),
+  // vowels: Ember.computed.filterBy('phonemes', 'syllabic', true),
+  // isValid: Ember.computed.notEmpty('name'),
+  // tds: Ember.computed.map('phoneme_ids', function(phoneme_id) {
+  //   return `#ph-${phoneme_id}`;
+  // }),
 });

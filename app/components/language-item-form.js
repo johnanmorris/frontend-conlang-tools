0396dddeb7864416cfe3626f558224cd43958b01
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-19 13:10:49 -0800

    comment cleanup

--- app/components/language-item-form.js
+++ app/components/language-item-form.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Component.extend({
-  store: Ember.inject.service(),
   buttonLabel: 'Save',
   phonemeIds: [],
   tds: [],
@@ -36,9 +35,6 @@ export default Ember.Component.extend({
   },
 
   click(event) {
-    // var store = this.get('store');
-    // var language = store.peekRecord('language', this.get('item.id'));
-    // var phonemes = language.get('phonemes');
     var clickedID = $(event.target).attr("id");
     if (clickedID === undefined) {
       clickedID = "ph-0";
@@ -46,7 +42,6 @@ export default Ember.Component.extend({
     var phoneID = parseInt(clickedID.slice(3));
 
     if(phoneID > 0) {
-      // var phone = store.findRecord('phoneme', phoneID);
       console.log("ID clicked: " + phoneID);
       $(event.target).toggleClass("selected-phone");
       if(this.get('phonemeIds').includes(phoneID)){
@@ -56,23 +51,6 @@ export default Ember.Component.extend({
       }
       console.log("phonemeIDS:");
       console.log(this.get('phonemeIds'));
-      // if(this.get('phonemeIds').includes(phoneID)){
-      //   console.log("remove!");
-      //   this.get('item.phonemeIds').removeObject(phoneID);
-      //
-      //   // language.get('phonemes').removeObject(phone);
-      // } else {
-      //   // this.get('phonemeIds').pushObject(phoneID);
-      //   console.log("add!");
-      //   // language.get("phonemes").pushObject(phone);
-      // }
-      // // if(phonemes.includes(phone)) {
-      // //   console.log("remove!");
-      // //   language.get('phonemes').removeObject(phone);
-      // // } else{
-      // //   console.log("add!");
-      // //   language.get('phonemes').pushObject(phone);
-      // // }
     }
   }
 });

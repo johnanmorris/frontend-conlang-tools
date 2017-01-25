80a58448f78d17b0867d9fd3a1b581b5fa4df33e
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-25 12:25:31 -0800

    add computed methods to define different types of consonants and vowels; improve UI and layout of show page

--- app/models/language.js
+++ app/models/language.js
@@ -8,7 +8,64 @@ export default DS.Model.extend({
   phoneme_ids: DS.attr(),
   words: DS.hasMany('word'),
 
+  isValid: Ember.computed.notEmpty('name'),
+
   consonants: Ember.computed.filterBy('phonemes', 'syllabic', false),
   vowels: Ember.computed.filterBy('phonemes', 'syllabic', true),
-  isValid: Ember.computed.notEmpty('name')
+
+  plosives: Ember.computed.filterBy('consonants', 'manner', 'plosive'),
+  nasals: Ember.computed.filterBy('consonants', 'manner', 'nasal'),
+  trills: Ember.computed.filterBy('consonants', 'manner', 'trill'),
+  taps: Ember.computed.filterBy('consonants', 'manner', 'tap'),
+  fricatives: Ember.computed.filterBy('consonants', 'manner', 'fricative'),
+  lateralFricatives: Ember.computed.filterBy('consonants', 'manner', 'lateral fricative'),
+  approximants: Ember.computed.filterBy('consonants', 'manner', 'approximant'),
+  lateralApproximants: Ember.computed.filterBy('consonants', 'manner', 'lateral approximant'),
+  affricates: Ember.computed.filterBy('consonants', 'manner', 'affricate'),
+  laterals: Ember.computed('lateralFricatives','lateralApproximants', function(){
+    var lats = [];
+    var fric = this.get('lateralFricatives');
+    var approx = this.get('lateralApproximants');
+    for(var i = 0; i < fric.length; i++) {
+      lats.push(fric[i]);
+    }
+    for (var j= 0; j < approx.length; j++) {
+      lats.push(approx[j]);
+    }
+    return lats;
+  }),
+
+  high: Ember.computed('vowels', function(){
+    var hi = [];
+    var vowels = this.get('vowels');
+    for(var i=0; i< vowels.length; i++){
+      if(vowels[i].high && !(vowels[i].low)) {
+        hi.push(vowels[i]);
+      }
+    }
+    return hi;
+  }),
+
+  mid: Ember.computed('vowels', function(){
+    var mid = [];
+    var vowels = this.get('vowels');
+    for(var i=0; i< vowels.length; i++){
+      if(!(vowels[i].high) && !(vowels[i].low)) {
+        mid.push(vowels[i]);
+      }
+    }
+    return mid;
+  }),
+
+  low: Ember.computed('vowels', function(){
+    var lo = [];
+    var vowels = this.get('vowels');
+    for(var i=0; i< vowels.length; i++){
+      if(!(vowels[i].high) && vowels[i].low) {
+        lo.push(vowels[i]);
+      }
+    }
+    return lo;
+  }),
+
 });
